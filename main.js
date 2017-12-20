9ac6b4ddc92c2a468dce27d276f98eff2c6ebf38
k725 <k725@users.noreply.github.com>
2017-12-21 00:10:19 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,28 @@
-(function(){
-	var itemPrice   = '[id^=itemPrice_]',
-		itemNoFound = '.itemAvailDetails',
-		itemPages   = '.num-pages',
-		totalCount  = 0,
-		totalError  = 0,
-		totalItem   = 0;
-
-	function wishGet() {
+(() => {
+	const s = document.createElement('script');
+	s.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js';
+	s.onload = () => {
+		let totalCount  = 0,
+			totalError  = 0,
+			totalItem   = 0;
 		if (document.URL.match(/..registry\/wishlist/)) {
-			alert('それなりの確率でコケます。1ページ目で実行してください。結果がおかしくなります。\n\n'+
-			'(めっちゃ重要)転んでも泣かない人向け。\n'+
-			'(とてもとても重要)旧バージョンなデザインだと動きません。');
+			alert('それなりの確率でコケます。urlを綺麗にしてから試してください。\ne.g. https://www.amazon.co.jp/registry/wishlist/2LQTBGUA6H6RT');
 
 			$.ajax({
-				url: document.URL + '?page=0'
-			}).done(function(data) {
+				url: `${document.URL}?page=0`
+			}).done((data) => {
 				try {
-					$($.parseHTML(data)).find(itemPrice).each(function() {
+					$($.parseHTML(data)).find('span.a-price-whole').each(function () {
 						totalCount += Number(new String($(this).text()).replace(/,/g, '').replace(/[^0-9]/g, ''));
 						totalItem++;
 					});
 
-					$($.parseHTML(data)).find(itemNoFound).each(function() {
+					$($.parseHTML(data)).find('.itemAvailMessage').filter('.a-text-bold').each(() => {
 						totalError++;
+						totalItem++;
 					});
 
-					alert('ほしい物リストの総額は...\n\n合計で' + totalCount + '円でした!! (在庫なし: ' + totalError + ' / 商品数: ' + totalItem + ')');
+					alert(`ほしい物リストの総額は...\n\n合計で${totalCount}円でした!! ([未加算]在庫なし: ${totalError} / 在庫あり: ${totalItem})`);
 				} catch(e) {
 					alert('エラーが発生しました。リロードし、再度お試しください。');
 				}
@@ -33,10 +30,6 @@
 		} else {
 			alert('Amazonのほしい物リストからお試しください');
 		}
-	}
-
-	var s = document.createElement('script');
-	s.src = '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js';
-	s.onload = wishGet;
+	};
 	document.body.appendChild(s);
 })();
